b4a9d22267b8846123012da08e314e0d4a61e867
YangXiangDong <yangxiangdong@lizhi.fm>
2019-09-16 20:46:41 +0800

    添加ip地址解析代码

---
+++ src/testArgs.java
@@ -0,0 +1,17 @@
+/**
+ * @Auther: Don
+ * @Date: 2019/9/10 14:47
+ * @Description:
+ */
+public class testArgs {
+    public static void main(String[] args) {
+//        String name = args[0];
+//        String location = args[1];
+        String a = "1";
+        double k = Double.valueOf(a);
+        double b = 1000000000;
+        double c = (k / b) * 20;
+//        System.out.println("name:" + name + "location:" + location);
+        System.out.println(c);
+    }
+}
